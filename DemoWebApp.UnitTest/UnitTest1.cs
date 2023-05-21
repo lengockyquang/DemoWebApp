978100f266e4f73f3ed4eb988e4a321582ce1d1d
lengockyquang <kyquang97@gmail.com>
2023-05-21 09:17:29 +0700

    Add unit test proj

---
+++ DemoWebApp.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DemoWebApp.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ThisTestShouldPassed()
+        {
+            Assert.True(true);
+        }
+    }
+}
